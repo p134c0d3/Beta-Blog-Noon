f03fb444f63329c693670ae5be051be3c28327a6
John P <70922864+p134c0d3@users.noreply.github.com>
2024-03-15 14:01:32 -0500

    Add Web Controller, Routes
    - Added the web controller
    - Updated the routes.rb to add the /web scope

---
+++ app/controllers/web_controller.rb
@@ -0,0 +1,6 @@
+class WebController < ApplicationController
+  before_action :authenticate_request
+  def bootstrap
+    render json: UserBlueprint.render(@current_user, view: :normal), status: :ok
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'web/bootstrap'
   post '/login', to: 'sessions#create'
   resources :blogs do
     post 'like'
     delete 'unlike'
   end
   resources :users, only: [:create]
+  scope '/web' do
+    get 'bootstrap', to: 'web#bootstrap'
+  end
 end
